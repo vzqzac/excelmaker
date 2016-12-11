ef52f86796f2e1a264ad53f9cfc838bebdd1412f
Omar Vazquez <omar.vazquezac@gmail.com>
2016-12-11 11:48:08 -0600

    heroku test

--- controllers/excelMaker.js
+++ controllers/excelMaker.js
@@ -3,6 +3,7 @@
  * function actionHandler() <- TODO
  */
 
+const json2xls = require('json2xls')
 const fbase = require('../fbase/handler')
 
 module.exports = {
@@ -11,8 +12,15 @@ module.exports = {
     let businessID = req.params.businessID
     let tableName = req.params.tableName
     let db = fbase[appName]()
-    db.ref().child('businesses/' + businessID + '/information/' + tableName).once('value').then(function (obj) {
-      res.send(obj.val())
+    db.ref().child('businesses/' + businessID + '/information/' + tableName).once('value').then(function (tableObj) {
+      let tableInfo = tableObj.val()
+      let jsonArr = []
+      for (var key in tableInfo) {
+        if (tableInfo.hasOwnProperty(key)) {
+          jsonArr.push(tableInfo[key])
+        }
+      }
+      res.xls('test.xlsx', jsonArr)
     })
   }
 }
--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = express()
 const bodyParser = require('body-parser')
 const firebase = require('./fbase/handler')
 const routes = require('./routes/index')
+const json2xls = require('json2xls');
 
 let port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+app.use(json2xls.middleware)
 
 firebase.initApps()
 
