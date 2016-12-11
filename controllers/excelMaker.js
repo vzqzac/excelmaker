26228ec26d026348b0a822e331f1679fe08583c2
Omar Vazquez <omar.vazquezac@gmail.com>
2016-12-11 16:22:58 -0600

    added table name and date for download

--- controllers/excelMaker.js
+++ controllers/excelMaker.js
@@ -28,7 +28,7 @@ module.exports = {
           jsonArr.push(prettyTitles)
         }
       }
-      res.xls('test.xlsx', jsonArr)
+      res.xls(appName + Date.now() + '.xlsx', jsonArr)
     })
   }
 }
