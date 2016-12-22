8c6ea741a73f9b34db778358f382ef68b33d59a0
Omar Vazquez <omar.vazquezac@gmail.com>
2016-12-22 17:03:17 -0600

    something

--- fbase/handler.js
+++ fbase/handler.js
@@ -1,23 +1,18 @@
 const admin = require('firebase-admin')
-const ezTableConfg = require('./config/ez-table.json')
-
-/*
- * Here comes the json file with admin privs
- */
-// var serviceAccount = require("path/to/serviceAccountKey.json")
+const ezTableConfg = require('./config/ez-table.json') // JSON config for ezTable firebase data
 
 // let inleteApp
 let ezTableApp
 
 module.exports = {
   initApps: function () {
-    // inleteApp = admin.initializeApp(/*inleteConfig*/, 'inlete')
+    // inleteApp = admin.initializeApp(inleteConfig, 'inlete')
     ezTableApp = admin.initializeApp({
       credential: admin.credential.cert(ezTableConfg),
       databaseURL: 'https://ez-table-80d9c.firebaseio.com'
     }, 'ez-table')
   },
-  // inlete: inleteApp.database(),
+  // inlete: () => inleteApp.database().ref(),
   ezTable: () => ezTableApp.database().ref(),
   fetchData: (db, childPath) => db.child(childPath).once('value').then((childObj) => childObj.val())
 }
