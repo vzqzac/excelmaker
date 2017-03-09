059b206b2e68de90dfd9fa8e22a6ebae07d697fe
Omar Vazquez <omar.vazquezac@gmail.com>
2017-03-09 16:14:06 -0600

    separated excel route to its won router

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const excelRouter = require('./excel')
 const excelMakerController = require('../controllers/excelMaker')
 
 module.exports = function (app) {
@@ -10,30 +11,30 @@ module.exports = function (app) {
     next()
   })
 
-  // Handle route to convert firebase data to CSV table object
-  router.route('/convert-table/:appName/:businessID/:tableName')
-    // Prepare data
-    .all(function (req, res, next) {
-      excelMakerController.getColumns(req, res)
-        .then(columns => {
-          // Sort columns as in DB
-          req.sortedColumns = excelMakerController.sortData(columns)
-          // Filter entities to handle before convert
-          req.entities = excelMakerController.filterEntities(columns)
-          return excelMakerController.getRows(req, res)
-        })
-        .then(rows => {
-          // Create CSV table
-          req.xlsTable = excelMakerController.convertTable(req, res, rows)
-          next()
-        })
-        .catch(error => res.status(500).send(error))
-    })
-    .get(excelMakerController.downloadTable)
-    .post(excelMakerController.emailTable)
-    .all(function (req, res) {
-      res.send({no: 'no'})
-    })
+  // Prepare data
+  router.use('*', function (req, res, next) {
+    excelMakerController.getColumns(req, res)
+      .then(columns => {
+        // Sort columns as in DB
+        req.sortedColumns = excelMakerController.sortData(columns)
+        // Filter entities to handle before convert
+        req.entities = excelMakerController.filterEntities(columns)
+        return excelMakerController.getRows(req, res)
+      })
+      .then(rows => {
+        // Create CSV table
+        req.xlsTable = excelMakerController.convertTable(req, res, rows)
+        next()
+      })
+      .catch(error => res.status(500).send(error))
+  })
+
+  router.use('/excel', excelRouter)
+  // router.use('/pdf', pdfRouter) <= tu router
+
+  router.all('*', function (req, res) {
+    res.status(400).send('Bad reqest')
+  })
 
   app.use('/', router)
 }
