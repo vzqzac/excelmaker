c9d5062a5b766a5a3aa795fa2973324f84581b50
Alexis\ONDECODE <alexis-yair-gonzalez@hotmail.com>
2017-03-09 17:46:22 -0600

    meaningless changes

--- controllers/pdfmaker.js
+++ controllers/pdfmaker.js
@@ -1,54 +1,61 @@
-var fs = require('fs');
-var path = require('path');
-var childProcess = require('child_process');
-var phantomjs = require('phantomjs');
-var binPath = phantomjs.path;
+const fs = require('fs');
+const path = require('path');
+const childProcess = require('child_process');
+const phantomjs = require('phantomjs');
+const binPath = phantomjs.path;
 
-var data = fs.readFileSync('./deal-2.csv', 'utf8');
+module.exports {
+    createHtmlTemplate: function () {
 
-var data = data.split("\n");
+        let data = fs.readFileSync('./deal-2.csv', 'utf8');
 
-for (var i = 0; i < data.length; i++) {
-    data[i] = data[i].split(",");
-}
+        let data = data.split("\n");
 
-fs.writeFileSync('htmlTable.html', ''); //cleaning html file
+        for (let i = 0; i < data.length; i++) {
+            data[i] = data[i].split(",");
+        }
 
-//Writing Html file. Creating Table
-var htmlString = '<!DOCTYPE html>' +
-    '<html><head><meta charset="UTF-8">' +
-    '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
-    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">' +
-    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>' +
-    '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>' +
-    '</head> <body><table class="table">';
-fs.appendFileSync('htmlTable.html', htmlString);
+        fs.writeFileSync('htmlTable.html', ''); //cleaning html file
 
-htmlString = '<thead><tr>';
-fs.appendFileSync('htmlTable.html', htmlString);
-for (var i = 0; i < data[0].length; i++) {
-    htmlString = "<th>" + data[0][i] + "</th>";
-    fs.appendFileSync('htmlTable.html', htmlString);
-}
-
-htmlString = '</tr></thead><tbody>';
-fs.appendFileSync('htmlTable.html', htmlString);
-for (var i = 1; i < data.length; i++) {
-    htmlString = '<tr>';
-    fs.appendFileSync('htmlTable.html', htmlString);
-    for (var j = 0; j < data[i].length; j++) {
-        htmlString = "<td>" + data[i][j] + "</td>";
+        //Writing Html file. Creating Table
+        let htmlString = '<!DOCTYPE html>' +
+            '<html><head><meta charset="UTF-8">' +
+            '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+            '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">' +
+            '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>' +
+            '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>' +
+            '</head> <body><table class="table">';
         fs.appendFileSync('htmlTable.html', htmlString);
-    }
-    htmlString = '</tr>';
-    fs.appendFileSync('htmlTable.html', htmlString);
-}
-htmlString = '</tbody> </table></body></html>';
-fs.appendFileSync('htmlTable.html', htmlString);
 
-var cp = childProcess.spawnSync(binPath, ['./renderpdf.js'], { //creating PDF Table.
-    cwd: process.cwd(),
-    env: process.env,
-    stdio: 'pipe',
-    encoding: 'utf-8'
-});
+        htmlString = '<thead><tr>'; //column titles
+        fs.appendFileSync('htmlTable.html', htmlString);
+        for (let i = 0; i < data[0].length; i++) {
+            htmlString = "<th>" + data[0][i] + "</th>";
+            fs.appendFileSync('htmlTable.html', htmlString);
+        }
+        htmlString = '</tr></thead><tbody>'; //rows
+        fs.appendFileSync('htmlTable.html', htmlString);
+        for (let i = 1; i < data.length; i++) {
+            htmlString = '<tr>';
+            fs.appendFileSync('htmlTable.html', htmlString);
+            for (let j = 0; j < data[i].length; j++) {
+                htmlString = "<td>" + data[i][j] + "</td>";
+                fs.appendFileSync('htmlTable.html', htmlString);
+            }
+            htmlString = '</tr>';
+            fs.appendFileSync('htmlTable.html', htmlString);
+        }
+        htmlString = '</tbody> </table></body></html>';
+        fs.appendFileSync('htmlTable.html', htmlString);
+    },
+    generatePdf: function () {
+        var cp = childProcess.spawnSync(binPath, ['./renderpdf.js'], { //creating PDF Table.
+            cwd: process.cwd(),
+            env: process.env,
+            stdio: 'pipe',
+            encoding: 'utf-8'
+        });
+    }
+}
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const excelMakerController = require('../controllers/excelMaker')
 
 module.exports = function (app) {
+  
   router.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
